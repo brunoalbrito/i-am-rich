b590cdb011aa305ef19b818ba0fa411239253256
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2026-09-14 20:31:55 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bruno Brito on 14/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
